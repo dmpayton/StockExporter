5a130d26db70e85cf1dd8e24ff6cd48f28166ee4
FrederickJ96 <fritzj96@gmail.com>
2018-02-25 00:59:17 -0800

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from alpha_vantage.timeseries import TimeSeries
+from openpyxl import Workbook
+import json
+# imports everthing I need
+wb = Workbook
+ws = wb.active
+ts = TimeSeries('N1RDW0MA9WZPJGCK')
+# sets a couple variables for libraries I'll need in a sec.
+data, meta_data = ts.get_daily(input(str()))
+close = data['2018-02-23']['1. open']
+print(close)
