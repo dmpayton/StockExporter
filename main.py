e4a68c4900d3bc5ca5dc8eda55e6bb4d9baf7f59
FrederickJ96 <fritzj96@gmail.com>
2018-02-26 13:18:06 -0800

    First working product. Many features missing but API is functional

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
+# imports everthing I need
 from alpha_vantage.timeseries import TimeSeries
 from openpyxl import Workbook
+import datetime
 import json
-# imports everthing I need
+# sets a couple variables for libraries I'll need in a sec.
 wb = Workbook
 ws = wb.active
 ts = TimeSeries('N1RDW0MA9WZPJGCK')
-# sets a couple variables for libraries I'll need in a sec.
+now = datetime.datetime.now()
+stringnow = str(now)
+todaysdate = (stringnow[:10])
+#Prints an intro message
+intromessage = """Stock Exporter V1.0
+This app takes data for securities with tickers, accepts input in the form of a
+ticker, and then creates an Excel document and writes the values for Open,
+Close, High, Low, and Volume into the appropriate cells.
+Type the Ticker for your desired security in now."""
+print(intromessage)
 data, meta_data = ts.get_daily(input(str()))
-close = data['2018-02-23']['4. close']
-print(close)
+openvalue = data[todaysdate]['1. open']
+highvalue = data[todaysdate]['2. high']
+lowvalue = data[todaysdate]['3. low']
+closevalue = data[todaysdate]['4. close']
+print("Open Price:" + openvalue)
+print("High Price:" + highvalue)
+print("Low Price:" + lowvalue)
+print("Close Price:" + closevalue)
